a98f453a031f0ea6629d448faf76f6610edb7875
Rui Pinheiro <rui.pinheiro@askblue.pt>
2024-10-18 17:01:26 +0100

    Initial commit

---
+++ ProductManagement/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ProductManagement.Models;
+
+namespace ProductManagement.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ ProductManagement/Models/ProductModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductManagement.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [StringLength(255)]
+        public string Description { get; set; }
+
+        [Range(0.01, 999999.99)]
+        public decimal Price { get; set; }
+    }
+}
